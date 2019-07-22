406a84b1a1d942f3884b2649ad22e9af1f747c63
rajeshsrinivasa1981 <ShreyaS2018>
2019-07-22 14:39:08 +0530

    Pacakges and Exceptions

--- Day10_Packages.py
+++ Day10_Packages.py
@@ -1,4 +1,4 @@
-'''
+
 
 #Approach 1
 
@@ -11,9 +11,9 @@ a_withFunction.display()
 
 import b_withFunction
 b_withFunction.show()
-'''
 
-'''
+
+
 #Approach 2
 
 import sys
@@ -26,8 +26,48 @@ display()
 sys.path.append("C:/Users/RS042424/PycharmProjects/POT1/pack2")
 from b_withFunction import *
 show()
-'''
+
+
+#Exception Handling
+
+""" 
+Multi except blocks...else..finally
+
+try:
+	statement;
+except Exception1 : #This will execute when Exception1 has occured 
+	----
+except Exception2 : #This will execute when Exception1 has occured
+	----
+except Exception3 : #This will execute when Exception1 has occured
+	----
+else:
+	------ //Executed else block when No exceptions found
+finally:
+	-------//This will execute everytime no matter what
+
+
+"""
+
+
+#try...except
+try:
+    num1,num2=10,0
+    result=num1/num2
+    print(result)
+except ZeroDivisionError as e:
+    print("ZeroDivisionError.... Exception occured and handled")
+except SyntaxError as s:
+    print("SyntaxError ..... Exception occured and Handled")
+except:
+    print("Invalid Input")
+else:
+    print("Entered into Else Block, No Exceptions found")
+finally:
+    print("Entered into Finally Block, program completed")
 
 
 
+#ZeroDivisionError
+#SyntaxError
 
